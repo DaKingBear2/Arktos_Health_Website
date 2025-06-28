50c5ab4b06c691f057efeec3b1b4ffb3980f01eb
DaKingBear <123269206+DaKingBear@users.noreply.github.com>
2025-06-28 21:15:21 +0000

    4th commit to new branch with 2 changes to simple texts but one .mjs texts

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://janedoe.com",
+  site: "https://Arktoshealth.com",
   base: "/",
   trailingSlash: "ignore",
   prefetch: {
